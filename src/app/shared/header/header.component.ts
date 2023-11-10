181580c2a06879e89404e88f0d4dadf82ff238ea
GoBedoya <diegobedoya.you@gmail.com>
2023-11-10 17:19:49 -0500

    Fin sección 4

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { InfoPaginaService } from '../../services/info-pagina.service';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
-
+  constructor( public _servicio: InfoPaginaService){}
 }
